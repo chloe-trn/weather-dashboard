0769a63558a78b1a9d94bdbaa6b548eb154f6d8e
chltrn <chltrn@users.noreply.github.com>
2020-12-28 10:06:42 -0500

    fix location toggle logic

--- scripts.js
+++ scripts.js
@@ -1,79 +1,97 @@
 // global variables:
 const APP_ID = '2e3b7f8f991531cb9a876edb9e0c1a22';
-let latitude = '';
-let longitude = '';
-var loc = 'New York'; // default location
-var userLoc = ''; 
-var temp = '';
-var desc = '';
+// search location and user location current states:
+let searchLocation = {
+  loc: 'New York',
+  fTemp:'',
+  desc:''
+};
+let userLocation ={
+  latitude: '',
+  longitude: '',
+  loc:'',
+  fTemp:'',
+  desc: ''
+};
+
+function checkUrl(url){
+  if(url == "urlLoc"){
+    let location = searchLocation.loc;
+    return `http://api.weatherstack.com/current?access_key=${APP_ID}&query=${location}`;
+  } else{ // "urlCoords"
+    let lat = userLocation.latitude;
+    let long = userLocation.longitude;
+    return `http://api.weatherstack.com/current?access_key=${APP_ID}&query=${lat},${long}`;
+  };
+}
+
+function setWeatherDisplay(inputUrl,location,temperature,description){
+  if(inputUrl == "urlLoc"){
+    searchLocation.loc = location;
+    searchLocation.fTemp = temperature;
+    searchLocation.desc = description;
+    $(".location").text(searchLocation.loc);
+    $(".temperature").text(searchLocation.fTemp);
+    $(".description").text(searchLocation.desc);
+  }else{// "urlCoords"
+    userLocation.loc = location;
+    userLocation.fTemp = temperature;
+    userLocation.desc = description;
+    $(".location").text(userLocation.loc);
+    $(".temperature").text(userLocation.fTemp);
+    $(".description").text(userLocation.desc);
+  }
+}
 
 // fetch from weather data WeatherStack API based on either location or coordinates:
 async function getWeatherData(url) {
-
-    var myUrl = "";
-    if(url == "urlLoc"){
-      myUrl = `http://api.weatherstack.com/current?access_key=${APP_ID}&query=${loc}`;
-    } else{ // "urlCoords"
-      myUrl = `http://api.weatherstack.com/current?access_key=${APP_ID}&query=${latitude},${longitude}`;
-    };
-
-    const response = await fetch(myUrl);
-    console.log("response:"+response);
-    const data = await response.json();
-    console.log("data:"+data);
-    loc = data.location.name;
-    temp = data.current.temperature; 
-    desc = data.current.weather_descriptions[0];
-    document.getElementById("location").innerHTML = loc;
-    document.getElementById("temperature").innerHTML = temp;
-    document.getElementById("description").innerHTML = desc;
+    let myUrl = checkUrl(url); // check what type of data we are pulling from API
+    const response = await fetch(myUrl); // fetch it
+    const data = await response.json(); // make it json format
+    let loc = data.location.name;   // pull necessary data
+    let temp = data.current.temperature;
+    let desc = data.current.weather_descriptions[0];
+    setWeatherDisplay(url,loc,temp,desc);  // display that data
 }
 
-// On submit button click (form submit), location is set to user input and global variables are set to new values: 
-function getLocation(){
-  loc = document.getElementById("search-text").value;
-  getWeatherData("urlLoc");
-}
-$('#form').submit(function(){
-  getLocation();
+// On submit button click (form submit), location is set to user input and global variables are set to new values:
+$("#form").submit(function(){
+  searchLocation.loc = document.getElementById("search-text").value;
+  getWeatherData("urlLoc"); // get and display input location
+  $("#search-text").val(""); // clear search
+  if($("#location-toggle").is(":checked")){
+    $("#location-toggle").prop( "checked", false );  // un-toggle
+  }
   return false; // prevents page refresh on form submit
 });
 
-// USER LOCATION FUNCTIONS: 
+// USER LOCATION FUNCTIONS:
 
 // checks if user has geolocation enabled:
 function getUserLocation() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(setUserCoords);
-  } else { 
-    // if false; display an alert to user ***************TO DO**********************8:
-    console.log("Geolocation is not supported by this browser.");
+  } else {
+    alert("Geolocation is not enabled or supported in your browser. Please enable geolocation or exit this alert.");
+    $("#location-toggle").prop( "checked", false );  // un-toggle
   }
 }
-
-// callback function runs if user geolocation is enabled: 
+// callback function runs if user geolocation is enabled:
 function setUserCoords(pos) {
   // sets user's coordinates(lattitude, longitude):
-  latitude = pos.coords.latitude; 
-  longitude = pos.coords.longitude; 
-  console.log("lat" + latitude);
-  console.log("long" + longitude);
-  // set global variables based on user's coordinates: 
-  getWeatherData("urlCoords");
+  userLocation.latitude = pos.coords.latitude;
+  userLocation.longitude = pos.coords.longitude;
+  getWeatherData("urlCoords"); // set global variables based on user's coordinates
 }
-
-$('input[name=checkbox]').change(function() {
-  if ($(this).is(':checked')) {
-    console.log("Checkbox is checked..")
+// listens for change in toggle checkbox for userlocation:
+$("input[id=location-toggle]").change(function() {
+  if ($(this).is(':checked')) { // if toggled
+    $("#search-text").val("");
     getUserLocation();
-  } else {
-    console.log("Checkbox is not checked..")
-    // switch weather display back to previous search/ current location stored in loc:
-
-    // ***********TODO***************** 
-    getWeatherData("urlLoc");  // last stored loc is user loc so this does not work, use 2 objects to store previous location instead?
+  } else { // if not toggled
+    getWeatherData("urlLoc"); // switch weather display back to previous search/ current location stored in loc
+    $("#search-text").val("");
   }
 });
 
 getWeatherData("urlLoc");
-
