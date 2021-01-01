0d05e55dbc33f11c0cb8ea3ef7246d25d97bcfc4
chltrn <chltrn@users.noreply.github.com>
2021-01-01 18:11:46 -0500

    add responsive design from desktop to mobile screen displays

--- scripts.js
+++ scripts.js
@@ -1,16 +1,19 @@
 // global variables:
 const APP_ID = "85e4a70f692bf0d169dde7822353cf50";
-let units = {temp:"f",wind:"mph",pres:"in"};
+let units = {temp:"f",wind:"mph"};
 // search location and user location current states:
+let validSearch = 0; // 0 - valid search, 1 - not valid search
+let prevValidLocation = "";
 let searchLocation = {
-  loc: "Honolulu",
+  loc: "Chicago",
   coun:"",
   temp:"",
   desc:"",
   feels:"",
   humid:"",
   pres:"",
-  wind:""
+  wind:"",
+  icon:""
 };
 let userLocation ={
   loc:"",
@@ -20,7 +23,29 @@ let userLocation ={
   feels:"",
   humid:"",
   pres:"",
-  wind:""
+  wind:"",
+  icon:""
+};
+let currentAnimation = "";
+let weatherDescriptions = { // icon-name : corresponding animation class
+  "01d":".day",
+  "01n":".night",
+  "02d":".cloudy-day",
+  "02n":".cloudy-night",
+  "03d":".cloudy",
+  "03n":".cloudy",
+  "04d":".cloudy",
+  "04n":".cloudy",
+  "09d":".rainy",
+  "09n":".rainy",
+  "10d":".rainy-day",
+  "10n":".rainy",
+  "11d":".thunder",
+  "11n":".thunder",
+  "13d":".snow",
+  "13n":".snow",
+  "50d":".mist",
+  "50n":".mist"
 };
 
 function checkUrl(url){
@@ -50,8 +75,9 @@ function setWeatherDisplay(inputUrl,newLocObj){
     $(".description").text(searchLocation.desc);
     $(".feelslike").text("Feels like: "+searchLocation.feels+"°"+units.temp.toUpperCase());
     $(".humidity").text(searchLocation.humid+" %");
-    $(".pressure").text(searchLocation.pres+" "+units.pres);
+    $(".pressure").text(searchLocation.pres+" inHg");
     $(".windspeed").text(searchLocation.wind+" "+units.wind);
+    $(".w-icon").attr("src", "http://openweathermap.org/img/w/" + searchLocation.icon + ".png");
   }else{// "urlCoords"
     userLocation = newLocObj;
     $(".location").text(userLocation.loc+", "+userLocation.coun);
@@ -59,26 +85,27 @@ function setWeatherDisplay(inputUrl,newLocObj){
     $(".description").text(userLocation.desc);
     $(".feelslike").text("Feels like: "+userLocation.feels+"°"+units.temp.toUpperCase());
     $(".humidity").text(userLocation.humid+" %");
-    $(".pressure").text(userLocation.pres+" "+units.pres);
+    $(".pressure").text(userLocation.pres+" inHg");
     $(".windspeed").text(userLocation.wind+" "+units.wind);
+    $(".w-icon").attr("src", "http://openweathermap.org/img/w/" + userLocation.icon + ".png");
   }
 }
 
 // fetch from weather data WeatherStack API based on either location or coordinates:
 async function getWeatherData(url) {
     let myUrl = checkUrl(url); // check what type of data we are pulling from API
-    /*TODO - Handling of unfulfilled promise from API */
     const response = await fetch(myUrl); // fetch it
-    console.log(response);
     const data = await response.json(); // make it json format
     console.log(data);
 
+    // pulling data is different depending on the query type urlLoc or urlCoords:
     if(url == "urlLoc"){
       if (data.count <= 0 ){
-        // make popup instead of alert
         alert("City entered is not supported, please enter another city. Ex: 'London' or 'Seoul'. Results are best with city name submitted not country name.");
-      }else {
-        //succesful location search:
+        validSearch = 1;
+        searchLocation.loc = prevValidLocation;
+      }else{//succesful location search:
+        validSearch = 0;
         let newData = {
           loc:data.list[0].name,   // pull necessary data
           coun:data.list[0].sys.country,
@@ -87,15 +114,21 @@ async function getWeatherData(url) {
           feels:Math.round(data.list[0].main.feels_like),
           humid:data.list[0].main.humidity,
           pres:data.list[0].main.pressure,
-          wind:Math.round(data.list[0].wind.speed)
+          wind:Math.round(data.list[0].wind.speed),
+          icon: data.list[0].weather[0].icon
         };
         setWeatherDisplay(url,newData);  // display that data
+        getAnimation(data.list[0].weather[0].icon);
       }
+      if($("#location-toggle").is(":checked") && validSearch == 0){ // if location is toggled and the search IS valid
+        $("#location-toggle").prop( "checked", false );  // un-toggle
+        $("label.loc-toggle").removeClass("translate-x");
+      }
+      prevValidLocation = data.list[0].name;  // set valid search as previous location for use if next searches are not valid
     }else{ // urlCoords
       if(data.cod === "400"){
         alert("Error. There is something wrong with the browser's geolocation functionality.");
       }else {
-        //successful geolocation search:
         let newData = {
           loc:data.name,   // pull necessary data
           coun:data.sys.country,
@@ -104,38 +137,32 @@ async function getWeatherData(url) {
           feels:Math.round(data.main.feels_like),
           humid:data.main.humidity,
           pres:data.main.pressure,
-          wind:Math.round(data.wind.speed)
+          wind:Math.round(data.wind.speed),
+          icon:data.weather[0].icon
         };
         setWeatherDisplay(url,newData);  // display that data
+        getAnimation(data.weather[0].icon);
       }
     }
-
-
 }
 // On submit button click (form submit), location is set to user input and global variables are set to new values:
 $("#form").submit(function(){
   searchLocation.loc = document.getElementById("search-text").value; // store input location
   getWeatherData("urlLoc"); // get and display input location
   $("#search-text").val(""); // clear search
-  if($("#location-toggle").is(":checked")){
-    $("#location-toggle").prop( "checked", false );  // un-toggle
-  }
   return false; // prevents page refresh on form submit
 });
 
 // Geolocation functions:
-if (!navigator.geolocation)
-{
+if (!navigator.geolocation){
     alert("Geolocation not available on your browser. Please exit alert or enable geolocation in your browser settings.");
 }
-function success(position)
-{
-    $(".latitude").val(position.coords.latitude);
-    $(".longitude").val(position.coords.longitude);
+function success(pos){
+    $(".latitude").val(pos.coords.latitude);
+    $(".longitude").val(pos.coords.longitude);
     getWeatherData("urlCoords"); // set global variables based on user's coordinates
 }
-function failure()
-{
+function failure(){
   alert("Geolocation not available on your browser. Please exit alert or enable geolocation in your browser settings.");
 }
 async function getUserLocation(){
@@ -148,10 +175,8 @@ $("input[id=location-toggle]").change(function() {
     $("#search-text").val("");
     $("label.loc-toggle").addClass("translate-x");
     if ($(".latitude").val() == ""){
-      console.log("let is undgwar");
       getUserLocation();
     }else{
-      console.log("not the first time");
       getWeatherData("urlCoords");
     }
   } else { // if not toggled
@@ -164,7 +189,7 @@ $("input[id=location-toggle]").change(function() {
 // listens for change in units toggle:
 $("input[id=units-toggle]").change(function() {
   if ($(this).is(':checked')) { // if toggled
-    let newUnits = {temp:"c",wind:"m/s",pres:"mm"}; // change units to Celsius
+    let newUnits = {temp:"c",wind:"m/s"}; // change units to Celsius
     units=newUnits;
     if($("#location-toggle").is(":checked")){ // check location toggle
       getWeatherData("urlCoords");
@@ -172,7 +197,7 @@ $("input[id=units-toggle]").change(function() {
       getWeatherData("urlLoc");
     }
   } else { // if not toggled
-    let newUnits = {temp:"f",wind:"mph",pres:"in"}; //change units to Farenheit
+    let newUnits = {temp:"f",wind:"mph"}; //change units to Farenheit
     units=newUnits;
     if($("#location-toggle").is(":checked")){ // check location toggle
       getWeatherData("urlCoords");
@@ -182,101 +207,23 @@ $("input[id=units-toggle]").change(function() {
   }
 });
 
-let currentAnimation = ".cloud";
-let weatherDescriptions = {
-  cloudy:".cloudy",
-  sunny:".day",
-  clear:".day",
-  mist:".mist",
-  fog:".mist",
-  hail:".hail",
-  "partly cloudy": ".partly-cloudy-day",
-  raining: ".medium-rain",
-  showers: ".medium-rain",
-  lightning:".thunder",
-  thunderstorm:".thunder",
-  snow:".medium-snow"
-};
-
-
-// animation function
+// Searches for and displays an animation:
 function getAnimation(weatherDesc){
-
-  // GET RID OF THIS:
-  $(".description").text(weatherDesc);
-
   let newAnimation = "";
   // turn off previous animation;
   for (let i = 0; i < currentAnimation.length; i++) {
-    console.log(currentAnimation);
     $(currentAnimation).css("display", "none");
   }
-  // check through each KEY in myList, if it matches something , append the VALUE into the newAnimation array
+  // check through each KEY in myList, if it matches something, set newAnimation
   if (weatherDescriptions.hasOwnProperty(weatherDesc)){
-    console.log(weatherDesc+" "+weatherDescriptions[weatherDesc]);
     newAnimation = weatherDescriptions[weatherDesc];
     currentAnimation = newAnimation; //set new animation state
-    console.log("newAnimation: "+newAnimation);
     $(newAnimation).css("display", "block"); // display weather animation found
   } else{
-    // do nothing , new animation is an empty string, input weatherDesc did not match anything in myList
-    //TODO ********************************************************************************************8
+    // if icon is not found
+    currentAnimation = ".else-case";
+    $(".else-case").css("display", "block");
   }
-
-
-  /*
-  if (newAnimation.length == 0){
-
-    console.log("Weather Description is not in myList");
-
-  }else if (newAnimation.length == 1) { //if array size ==1, just do a normal display ; cloudy, sunny , mist/fog
-
-    if(weatherDesc =="cloudy"){
-      console.log("newAnimation[0]: "+newAnimation[0]);
-      $(newAnimation[0]).removeClass("cloud-position");
-      $(newAnimation[0]).addClass("cloud-animation");
-      $(newAnimation[0]).css("display", "block"); // display cloud
-    }else if (weatherDesc =="sunny") { // sunny ; elseif
-      $(newAnimation[0]).removeClass("sun-position");
-      $(newAnimation[0]).addClass("sun-animation");
-      $(newAnimation[0]).css("display", "block"); // display cloud
-    } else{ // else; mist
-      $(newAnimation[0]).css("display", "block"); // display mist
-    }
-
-  }else if (newAnimation.length == 2) { // array.length == 2, display it differently ; partly cloudy; rainy; lightning
-    if(weatherDesc =="partly cloudy"){
-      console.log("newAnimation[0]: "+newAnimation[0]);
-      console.log("newAnimation[1]: "+newAnimation[1]);
-      $(newAnimation[0]).addClass("cloud-position");
-      $(newAnimation[0]).addClass("cloud-animation");
-      $(newAnimation[0]).css("display", "block");
-      $(newAnimation[1]).addClass("sun-position");
-      $(newAnimation[1]).addClass("sun-animation");
-      $(newAnimation[1]).css("display", "block");
-    }else if (weatherDesc =="raining"||weatherDesc =="showers"){
-      console.log("raining");
-      $(newAnimation[0]).removeClass("cloud-position");
-      $(newAnimation[0]).css("display", "block"); //display cloud
-      $(newAnimation[1]).css("display", "block"); // display rain
-    }else{ //lightning
-      console.log("lightning");
-      $(newAnimation[0]).removeClass("cloud-animation");
-      $(newAnimation[0]).removeClass("cloud-position");
-      $(newAnimation[0]).css("display", "block"); //display cloud
-      $(newAnimation[1]).css("display", "block"); //display lightning
-    }
-
-  }else{ // array.length === 3 or more; thunderstorm
-    if(weatherDesc =="thunderstorm"){
-      $(newAnimation[0]).removeClass("cloud-animation");
-      $(newAnimation[0]).removeClass("cloud-position");
-      $(newAnimation[0]).css("display", "block"); //display cloud
-      $(newAnimation[1]).css("display", "block"); //display lightning
-      $(newAnimation[2]).css("display", "block"); //display rain
-    }
-  }*/
-
 }
 
 $( document ).ready(function() {
