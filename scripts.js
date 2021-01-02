565a8dce5eb6239048046f88680b96139239d7e9
chltrn <chltrn@users.noreply.github.com>
2021-01-01 19:08:32 -0500

    update comments to be more clear

--- scripts.js
+++ scripts.js
@@ -1,9 +1,11 @@
 // global variables:
-const APP_ID = "85e4a70f692bf0d169dde7822353cf50";
-let units = {temp:"f",wind:"mph"};
-// search location and user location current states:
-let validSearch = 0; // 0 - valid search, 1 - not valid search
+const APP_ID = "85e4a70f692bf0d169dde7822353cf50"; // API key
+let units = {temp:"f",wind:"mph"};  // default units
+let cUnits = {temp:"c",wind:"m/s"};
+let fUnits = {temp:"f",wind:"mph"};
+let validSearch = 0; // 0 - valid search; 1 - not valid search
 let prevValidLocation = "";
+// search location and user location current states:
 let searchLocation = {
   loc: "Chicago",
   coun:"",
@@ -47,7 +49,7 @@ let weatherDescriptions = { // icon-name : corresponding animation class
   "50d":".mist",
   "50n":".mist"
 };
-
+// return the API call depending on if user's location toggle is on and what units are chosen
 function checkUrl(url){
   if(url == "urlLoc"){
     let location = searchLocation.loc;
@@ -66,7 +68,7 @@ function checkUrl(url){
     }
   };
 }
-
+// Set the DOM with values found from the API call
 function setWeatherDisplay(inputUrl,newLocObj){
   if(inputUrl == "urlLoc"){
     searchLocation = newLocObj;
@@ -90,24 +92,22 @@ function setWeatherDisplay(inputUrl,newLocObj){
     $(".w-icon").attr("src", "http://openweathermap.org/img/w/" + userLocation.icon + ".png");
   }
 }
-
-// fetch from weather data WeatherStack API based on either location or coordinates:
+// Fetch from weather data OpenWeather API based on either location or coordinates
 async function getWeatherData(url) {
-    let myUrl = checkUrl(url); // check what type of data we are pulling from API
-    const response = await fetch(myUrl); // fetch it
-    const data = await response.json(); // make it json format
+    let myUrl = checkUrl(url);            // check what type of data we are pulling from API
+    const response = await fetch(myUrl);  // fetch it
+    const data = await response.json();   // make it json
     console.log(data);
-
-    // pulling data is different depending on the query type urlLoc or urlCoords:
-    if(url == "urlLoc"){
-      if (data.count <= 0 ){
+    // pulling data process is different depending on the query type urlLoc or urlCoords:
+    if(url == "urlLoc"){  // user enters location:
+      if (data.count <= 0 ){             // user entered invalid city
         alert("City entered is not supported, please enter another city. Ex: 'London' or 'Seoul'. Results are best with city name submitted not country name.");
-        validSearch = 1;
-        searchLocation.loc = prevValidLocation;
-      }else{//succesful location search:
-        validSearch = 0;
-        let newData = {
-          loc:data.list[0].name,   // pull necessary data
+        validSearch = 1;                // not a valid search
+        searchLocation.loc = prevValidLocation;    // set stored object to the previous valid search
+      }else{
+        validSearch = 0; // valid search
+        let newData = {  // pull necessary data in object
+          loc:data.list[0].name,
           coun:data.list[0].sys.country,
           temp:Math.round(data.list[0].main.temp),
           desc:data.list[0].weather[0].main,
@@ -117,15 +117,15 @@ async function getWeatherData(url) {
           wind:Math.round(data.list[0].wind.speed),
           icon: data.list[0].weather[0].icon
         };
-        setWeatherDisplay(url,newData);  // display that data
-        getAnimation(data.list[0].weather[0].icon);
+        setWeatherDisplay(url,newData);// pass object to display data on DOM
+        getAnimation(data.list[0].weather[0].icon); // set animation on DOM based on data
       }
       if($("#location-toggle").is(":checked") && validSearch == 0){ // if location is toggled and the search IS valid
-        $("#location-toggle").prop( "checked", false );  // un-toggle
+        $("#location-toggle").prop( "checked", false );  // un-toggle location toggle
         $("label.loc-toggle").removeClass("translate-x");
       }
       prevValidLocation = data.list[0].name;  // set valid search as previous location for use if next searches are not valid
-    }else{ // urlCoords
+    }else{ // urlCoords, user toggles location:
       if(data.cod === "400"){
         alert("Error. There is something wrong with the browser's geolocation functionality.");
       }else {
@@ -140,27 +140,27 @@ async function getWeatherData(url) {
           wind:Math.round(data.wind.speed),
           icon:data.weather[0].icon
         };
-        setWeatherDisplay(url,newData);  // display that data
-        getAnimation(data.weather[0].icon);
+        setWeatherDisplay(url,newData);     // pass object to display data on DOM
+        getAnimation(data.weather[0].icon); // set animation on DOM based on data
       }
     }
 }
-// On submit button click (form submit), location is set to user input and global variables are set to new values:
+// On form submit, city input is set as location variable and API data is fetched
 $("#form").submit(function(){
-  searchLocation.loc = document.getElementById("search-text").value; // store input location
-  getWeatherData("urlLoc"); // get and display input location
+  searchLocation.loc = document.getElementById("search-text").value; // set input city
+  getWeatherData("urlLoc");  // get and display input city to DOM
   $("#search-text").val(""); // clear search
-  return false; // prevents page refresh on form submit
+  return false; // prevent page refresh on form submit
 });
 
-// Geolocation functions:
+// Geolocation functions to fetch user coordinates
 if (!navigator.geolocation){
     alert("Geolocation not available on your browser. Please exit alert or enable geolocation in your browser settings.");
 }
 function success(pos){
     $(".latitude").val(pos.coords.latitude);
     $(".longitude").val(pos.coords.longitude);
-    getWeatherData("urlCoords"); // set global variables based on user's coordinates
+    getWeatherData("urlCoords");
 }
 function failure(){
   alert("Geolocation not available on your browser. Please exit alert or enable geolocation in your browser settings.");
@@ -168,64 +168,57 @@ function failure(){
 async function getUserLocation(){
   navigator.geolocation.getCurrentPosition(success,failure);
 }
-
-// listens for change in toggle checkbox for userlocation:
+// Listen for change in toggle for location
 $("input[id=location-toggle]").change(function() {
   if ($(this).is(':checked')) { // if toggled
-    $("#search-text").val("");
-    $("label.loc-toggle").addClass("translate-x");
-    if ($(".latitude").val() == ""){
-      getUserLocation();
-    }else{
-      getWeatherData("urlCoords");
+    $("#search-text").val("");                      // clear search field
+    $("label.loc-toggle").addClass("translate-x");  // move toggle to right
+    if ($(".latitude").val() == ""){                // if this is the first time location is toggled
+      getUserLocation();                            // use Geolocation to get user coordinates
+    }else{  // not the first time location is toggled
+      getWeatherData("urlCoords");                  // fetch data based on coordinates and display on DOM
     }
   } else { // if not toggled
-    getWeatherData("urlLoc"); // switch weather display back to previous search/ current location stored in loc
-    $("#search-text").val("");
-    $("label.loc-toggle").removeClass("translate-x");
+    getWeatherData("urlLoc");                      // switch weather display back to previous search
+    $("#search-text").val("");                     // clear search field
+    $("label.loc-toggle").removeClass("translate-x");  // move toggle to left
   }
 });
-
-// listens for change in units toggle:
+// listen for change in toggle for units
 $("input[id=units-toggle]").change(function() {
   if ($(this).is(':checked')) { // if toggled
-    let newUnits = {temp:"c",wind:"m/s"}; // change units to Celsius
-    units=newUnits;
-    if($("#location-toggle").is(":checked")){ // check location toggle
+    units=cUnits;                              // update to cUnits
+    if($("#location-toggle").is(":checked")){  // check location toggle -> update DOM
       getWeatherData("urlCoords");
     } else{
       getWeatherData("urlLoc");
     }
   } else { // if not toggled
-    let newUnits = {temp:"f",wind:"mph"}; //change units to Farenheit
-    units=newUnits;
-    if($("#location-toggle").is(":checked")){ // check location toggle
+    units=fUnits;                             // update to fUnits
+    if($("#location-toggle").is(":checked")){ // check location toggle -> update DOM
       getWeatherData("urlCoords");
     } else{
       getWeatherData("urlLoc");
     }
   }
 });
-
-// Searches for and displays an animation:
+// Search for and display animation
 function getAnimation(weatherDesc){
   let newAnimation = "";
-  // turn off previous animation;
+  // turn off previous animations
   for (let i = 0; i < currentAnimation.length; i++) {
     $(currentAnimation).css("display", "none");
   }
-  // check through each KEY in myList, if it matches something, set newAnimation
+  // check through each key in list, if it matches something, set newAnimation
   if (weatherDescriptions.hasOwnProperty(weatherDesc)){
     newAnimation = weatherDescriptions[weatherDesc];
-    currentAnimation = newAnimation; //set new animation state
-    $(newAnimation).css("display", "block"); // display weather animation found
-  } else{
-    // if icon is not found
+    currentAnimation = newAnimation;         // set new animation state
+    $(newAnimation).css("display", "block"); // display new animation
+  } else{ // if animation is not found
     currentAnimation = ".else-case";
     $(".else-case").css("display", "block");
   }
 }
-
-$( document ).ready(function() {
-    getWeatherData("urlLoc");
+$( document ).ready(function() { // on document load
+    getWeatherData("urlLoc");    // fetch and display data to DOM for default location of Chicago
 });
